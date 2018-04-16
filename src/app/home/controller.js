b513ee2e1292a97d51bbc381e33e36899ecc93d3
zoujirong <1298452397@qq.com>
2018-04-17 00:03:58 +0800

    #标大大前端项目 企信资质信息

--- src/app/home/controller.js
+++ src/app/home/controller.js
@@ -2,10 +2,9 @@
 	angular
 				.module('HomeController',['ui.bootstrap','ngTouch'])
 				.controller('HomeCtrl', ['$http','$uibModal','$log','$scope','$document',function($http,$uibModal, $log, $scope,$document) {
-					var selt = this;
-					this.cancelAreaB=function(){
-
-					}
+                    $http.post("/foundation/listBannerImage",{type:1,showType:1}).success(function (result) {
+                        console.log(result);
+                    });
 
 				}]);
 
