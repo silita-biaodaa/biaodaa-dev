7133cb6b55a3204fa22334d7df8b7c7a28c06151
zhangxiahui <281055769@qq.com>
2018-04-17 17:36:02 +0800

    #标大大前端项目 企业详情

--- src/app/companydetail/controller.js
+++ src/app/companydetail/controller.js
@@ -9,6 +9,31 @@ app.controller('CompanyDetailCtrl', ['$http','$scope','utils',function($http,$sc
 		selt.company=result.data;
 	});
 
+	this.showType = 1;
+	var style00 = {
+		"background-color":"#4d3a3a",
+		"color": "white"
+	};
+	this.style01 = style00;
+
+
+	this.changeType = function (type) {
+		selt.showType = type;
+		selt.style01 = "";
+		selt.style02 = "";
+		selt.style03 = "";
+		selt.style04 = "";
+		if(type==1){
+			selt.style01 = style00;
+		}else if(type==2){
+			selt.style02 = style00;
+		}else if(type==3){
+			selt.style03 = style00;
+		}else if(type==4){
+			selt.style04 = style00;
+		}
+	}
+
 
 
 
