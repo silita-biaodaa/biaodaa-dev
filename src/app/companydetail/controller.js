c37af5f74cf27d3a8af1fd07b9159f137545d9d2
zhangxiahui <281055769@qq.com>
2018-04-17 16:41:45 +0800

    #标大大前端项目 企业详情

--- src/app/companydetail/controller.js
+++ src/app/companydetail/controller.js
@@ -1,59 +1,24 @@
-(function() {
-	angular
-		.module('CompanyDetailController', ['ui.bootstrap'])
-		.controller('CompanyDetailCtrl', ['$http','$uibModal','$log','$document',function($http,$uibModal, $log, $document) {
-			var selt = this;
+app.controller('CompanyDetailCtrl', ['$http','$scope','utils',function($http,$scope, utils) {
+	var selt = this;
 
-			this.title = 'INDEX1111';
-			this.content = 'This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.';
+	var comId =  utils.getUrlVar('comId');
+	console.log("=====comId:"+comId);
 
-			this.getTest = function (comId){
-				var params = {};
-				$http.post("/company/"+comId,angular.toJson(params)).success(function (result) {
-					selt.company=result.data;
-				});
-
-			};
-
-			this.setPage = function (pageNo) {
-				var paramsPage = {
-					keyWord:'湖南',
-					pageNo:pageNo,
-					pageSize:5
-				};
-				$http.post("/company/query",angular.toJson(paramsPage)).success(function (result) {
-					selt.testList = result.data;
-					selt.totalCount = result.total;
-					selt.pageSize = result.pageSize;;
-					selt.pageNo = result.pageNum;
-				});
-			};
-
-			this.pageChanged = function() {
-				var paramsPage = {
-					keyWord:'湖南',
-					pageNo:this.pageNo,
-					pageSize:5
-				};
-				$log.log('Page changed to: ' + this.pageNo);
-				$http.post("/company/query",angular.toJson(paramsPage)).success(function (result) {
-					selt.testList = result.data;
-					selt.totalCount = result.total;
-					selt.pageSize = result.pageSize;;
-					selt.pageNo = result.pageNum;
-				});
-
-			};
-
-			this.maxSize = 3;
-			this.setPage(1);
+	var params = {};
+	$http.post("/company/"+comId,angular.toJson(params)).success(function (result) {
+		selt.company=result.data;
+	});
 
 
 
 
 
-		}]);
 
 
 
-})();
+
+
+
+
+
+}]);
--- src/app/personalinfo/controller.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	angular
-		.module('PersonInfoController',['ui.bootstrap','ngTouch'])
-		.controller('PersonInfoCtrl', ['$http','$uibModal','$log','$scope','$document',function($http,$uibModal, $log, $scope,$document) {
-
-		}]);
-
-})();
