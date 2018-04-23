27c3a6ad0eb8fc0c37b758bdb52bf55fac67be29
ddd0401 <ddd0401110@sina.com>
2018-04-23 17:57:41 +0800

    #标大大前端项目 首页列表请求加入

--- src/app/home/controller.js
+++ src/app/home/controller.js
@@ -9,16 +9,25 @@ app.controller('HomeCtrl', ['$http','$uibModal','$log','$scope','$document', 'us
 		selt.user = null;
 	}
 
-	var paramsPage = {
-		type:2,
-		pageNo:1,
-		pageSize:5
-	};
 
-	$http.post("/notice/queryList",angular.toJson(paramsPage)).success(function (result) {
-		console.log(result);
-		selt.testList = result.data;
-	});
+	//公告文章列表
+	this.queryArticleList = function (type){
+		var articleParam ={
+			pageNo:1,
+			pageSize:5,
+			type:type
+		};
+		$http.post("/notice/queryArticleList",angular.toJson(articleParam)).success(function (result) {
+			console.log(result);
+			if(articleParam.type==0){
+				selt.tradeList = result.data;
+			}else if(articleParam.type==1){
+				selt.platformList = result.data;
+			}
+		});
+	}
+	this.queryArticleList('0');
+	this.queryArticleList('1');
 
 	this.setPage = function () {
 		var paramsPage = {
@@ -30,16 +39,20 @@ app.controller('HomeCtrl', ['$http','$uibModal','$log','$scope','$document', 'us
 
 		$http.post("/notice/queryList",angular.toJson(paramsPage)).success(function (result) {
 			console.log(result);
-			selt.testList = result.data;
+			if(paramsPage.type==0){
+				selt.zhaobiaoList = result.data;
+			}else if(paramsPage.type==2){
+				selt.zhongbiaoList = result.data;
+			}
 		});
 	};
+
 
 	this.choiceType = function (type,projectType) {
 		selt.type=type;
 		selt.projectType = projectType;
 		selt.setPage()
 	}
-
 
 	if(username != null && username != '') {
 		selt.user = {
@@ -61,4 +74,8 @@ app.controller('HomeCtrl', ['$http','$uibModal','$log','$scope','$document', 'us
 		window.location.href="index.html#/home";
     };
 
+
+	//页面初始化请求
+	this.choiceType(2,'');
+	this.choiceType(0,'');
 }]);
