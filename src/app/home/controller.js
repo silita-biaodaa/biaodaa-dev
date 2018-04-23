b6a5627f0f33148054f02d3f8ea5c5569fa20322
zoujirong <1298452397@qq.com>
2018-04-23 10:36:02 +0800

    #标大大前端项目

--- src/app/home/controller.js
+++ src/app/home/controller.js
@@ -1,25 +1,6 @@
-app.controller('HomeCtrl', ['$http','$uibModal','$log','$scope','$document', 'username',function($http,$uibModal, $log, $scope, $document, username) {
-	var selt = this;
-
-
-	if(username != null && username != '') {
-		selt.user = {
-			username : username
-		};
-	} else {
-		selt.user = null;
-	}
-
+app.controller('HomeCtrl', ['$http','$uibModal','$log','$scope','$document',function($http,$uibModal, $log, $scope,$document) {
 	$http.post("/foundation/listBannerImage",{type:1,showType:1}).success(function (result) {
 		console.log(result);
 	});
-
-	this.logout = function() {
-		sessionStorage.removeItem("X-TOKEN");
-		sessionStorage.removeItem("username");
-		username = "";
-		selt.user = null;
-		window.location.href="index.html#/home";
-	}
 
 }]);
