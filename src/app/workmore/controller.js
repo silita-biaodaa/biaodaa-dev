e249f7bab31b395a7108fd5f25e4c1f477a787e5
zhangxiahui <281055769@qq.com>
2018-04-26 11:11:48 +0800

    #标大大前端系统 平台公示和平台资讯列表分页

--- src/app/workmore/controller.js
+++ src/app/workmore/controller.js
@@ -7,35 +7,56 @@ app.controller('WorkMoreCtrl', ['$http','$uibModal','$log','$scope','$state','lo
     } else {
         selt.user = null;
     }
-    $scope.isCompany = true;
+
+
+    this.type = '0';
+    this.dataArray = [];
+    this.busy = false;
+    this.page = 1;
+
+    this.nextPage = function () {
+        if (selt.busy) return;
+        selt.busy = true;
         var params = {
-            pageNo:1,
+            pageNo:selt.page,
             pageSize:20,
             time:'',
             title:'',
-            type:'0'
-        }
+            type:selt.type
+        };
 
-        $http.post("/notice/queryArticleList",params).success(function (result) {
-                console.log(result);
-                $scope.dataArray = result.data;
-            });
-        $scope.toDiffMsg = function(type){
-            params.type=type;
-            if(type=='0'){
-                $scope.isCompany = true;
-            }else{
-                $scope.isCompany = false;
+        $http.post("/notice/queryArticleList",angular.toJson(params)).success(function (result) {
+            var dataArray = result.data;
+            if(dataArray!=null&&selt.page==result.pageNum){
+                angular.forEach(dataArray,function(item){
+                    selt.dataArray.push(item);
+                });
+                selt.busy = false;
+                selt.page += 1;
             }
-            $http.post("/notice/queryArticleList",params).success(function (result) {
-                console.log(result);
-                $scope.dataArray = result.data;
-            });
-        }
-        $scope.toDetail = function(id){
-            $state.go('workmoreDetail',{id:id});
 
+
+        });
+    };
+
+    this.isCompany = true;
+
+    this.toDiffMsg = function(type){
+        selt.type=type;
+        if(type=='0'){
+            selt.isCompany = true;
+        }else{
+            selt.isCompany = false;
         }
+        selt.dataArray = [];
+        selt.busy = false;
+        selt.page = 1;
+        selt.nextPage();
+    }
+    this.toDetail = function(id){
+        $state.go('workmoreDetail',{id:id});
+
+    }
     this.logout = function () {
         sessionStorage.removeItem("X-TOKEN");
         sessionStorage.removeItem("userTemp");
