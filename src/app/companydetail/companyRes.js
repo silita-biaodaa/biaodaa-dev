d39c18c466c06ca30d82127d3f6007537c9d442f
zoujirong <1298452397@qq.com>
2018-05-14 15:14:10 +0800

    标大大网页端样式修改

--- src/app/companydetail/companyRes.js
+++ src/app/companydetail/companyRes.js
@@ -5,7 +5,8 @@ app.controller('CompanyResCtrl', ['$http','$uibModal','$log','$scope','$document
     } else {
         self.user = null;
     }
-
+    $location.hash();
+    $anchorScroll();
     /**
      * 强制刷新用户状态
      */
